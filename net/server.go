efbe524b7665e0b90b0bb5dbfcd85722a569bccf
coco <coco@aaa.com>
2022-08-10 12:28:59 +0800

    server

---
+++ net/server.go
@@ -0,0 +1,25 @@
+package net
+
+import "github.com/cocomylove/tcpserver/iface"
+
+// 服务器
+type Server struct {
+	//服务器的名称
+	Name string
+	//tcp4 or other
+	IPVersion string
+	//服务绑定的IP地址
+	IP string
+	//服务绑定的端口
+	Port int
+	//当前Server的消息管理模块，用来绑定MsgID和对应的处理方法
+	msgHandler iface.IMsgHandler
+	//当前Server的链接管理器
+	ConnMgr iface.IConnManager
+	//该Server的连接创建时Hook函数
+	OnConnStart func(conn iface.IConnection)
+	//该Server的连接断开时的Hook函数
+	OnConnStop func(conn iface.IConnection)
+
+	packet iface.IDataPack
+}
