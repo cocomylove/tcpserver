589f7dc8f4fa648a88095ad5df6689aa91546f09
coco <coco@aaa.com>
2022-08-12 22:48:56 +0800

    全部实现

---
+++ net/router.go
@@ -0,0 +1,11 @@
+package net
+
+import "github.com/cocomylove/tcpserver/iface"
+
+type BaseRouter struct{}
+
+func (br *BaseRouter) PreHandle(req iface.IRequest) {}
+
+func (br *BaseRouter) Handle(req iface.IRequest) {}
+
+func (br *BaseRouter) PostHandle(req iface.IRequest) {}
